ad1852cae77f6dbb3fe87d515dae6d01d1f0b0ba
Zach <zacharymd14@gmail.com>
2024-10-19 22:43:37 +0200

    Basic stuff

--- src/main/java/com/TimerApp/zachary/App.java
+++ src/main/java/com/TimerApp/zachary/App.java
@@ -1,13 +1,17 @@
 package com.TimerApp.zachary;
 
+import javax.swing.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+
+	}
 }
